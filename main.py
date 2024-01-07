fbccc9a51bf07f62ba89315193b75e62a2ab3bc6
Egor <egpegpegp1805002000@gmail.com>
2024-01-07 18:13:37 +0300

    +bash sc and req.txt

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.templating import Jinja2Templates
 import os
 
-host = os.getenv("HOST")
+host = 'clickhouse-service'
+#os.getenv("HOST")
 templates = Jinja2Templates(directory="templates")
 
 client = clickhouse_connect.get_client(host=host)
--- terr/doc/main.Dockerfile
+++ terr/doc/main.Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.10.13-bullseye
+ENV DB_HOST=clickhouse-service
 
 RUN apt-get install git -y
 RUN git clone https://github.com/Egorlop/CloudFAST
 WORKDIR /CloudFAST
 RUN pip install -r requirements.txt
+
+CMD ["bash", "-c", "uvicorn main:app --host 0.0.0.0 --port 8080"]
